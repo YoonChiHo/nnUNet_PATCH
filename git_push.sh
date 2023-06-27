99d49a12f95d2bf0b877a76698a62029fdcde630
YoonChiHo <sciencedbs@gmail.com>
2023-06-27 13:30:47 +0900

    20230627_update

--- git_push.sh
+++ git_push.sh
@@ -4,14 +4,14 @@ today=$(date "+%Y%m%d")
 # Get private git code also
 git config --global user.email "sciencedbs@gmail.com"
 git config --global user.name "YoonChiHo"
-git config --global user.password "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"
+git config --global user.password "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"
 
 # Push
 git add *
 git commit -m ${today}'_update'
-git push https://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@github.com/YoonChiHo/nnUNet_PATCH.git main
+git push https://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@github.com/YoonChiHo/nnUNet_PATCH.git main
 
 echo PUSH ${today}_update Completed
 
 # Pull
-#git pull https://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@github.com/YoonChiHo/nnUNet_PATCH.git main
+#git pull https://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@github.com/YoonChiHo/nnUNet_PATCH.git main
