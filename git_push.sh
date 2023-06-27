4f83068a5555c8b1d96e3ac1915e6df10e445ec8
YoonChiHo <sciencedbs@gmail.com>
2023-06-27 13:21:17 +0900

    20230627_update

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,10 @@
 today=$(date "+%Y%m%d")
 
+# Get private git code also
+git config --global user.email "sciencedbs@gmail.com"
+git config --global user.name "YoonChiHo"
+git config --global user.password "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"
+
 # Push
 git add *
 git commit -m ${today}'_update'
